15c7f1184d4695238c8f66286d0e031426934dbe
Erik Wengle <erik.wengle@bint.ch>
2025-01-12 19:56:59 +0100

    wowie

--- extra/generate.py
+++ extra/generate.py
@@ -4,23 +4,22 @@
 string = '''{
     "parent": "minecraft:item/generated",
     "textures": {
-      "layer0": "mm/items/event/ITEM_NAME"
+      "layer0": "mm/items/ITEM_NAME"
     }
   }'''
 
 
-    # { "predicate": { "custom_model_data": 3000100}, "model": "mm/items/event/express_mail"},
-    # { "predicate": { "custom_model_data": 3000101}, "model": "mm/items/event/kafei_letter"},
-    # { "predicate": { "custom_model_data": 3000102}, "model": "mm/items/event/land_td"},
-    # { "predicate": { "custom_model_data": 3000103}, "model": "mm/items/event/moons_tear"},
-    # { "predicate": { "custom_model_data": 3000104}, "model": "mm/items/event/mountain_td"},
-    # { "predicate": { "custom_model_data": 3000105}, "model": "mm/items/event/ocean_td"},
-    # { "predicate": { "custom_model_data": 3000106}, "model": "mm/items/event/pendant"},
-    # { "predicate": { "custom_model_data": 3000107}, "model": "mm/items/event/room_key"},
-    # { "predicate": { "custom_model_data": 3000108}, "model": "mm/items/event/swamp_td"},
 
 
-item_names = ['express_mail', 'kafei_letter', 'land_td', 'moons_tear', 'mountain_td', 'ocean_td', 'pendant', 'room_key', 'swamp_td']
+    # { "predicate": { "custom_model_data": 8000017}, "model": "mm/items/bomb_none"},
+    # { "predicate": { "custom_model_data": 8000018}, "model": "mm/items/bombchu_none"},
+    # { "predicate": { "custom_model_data": 8000019}, "model": "mm/items/deku_stick_none"},
+    # { "predicate": { "custom_model_data": 8000020}, "model": "mm/items/deku_nut_none"},
+    # { "predicate": { "custom_model_data": 8000021}, "model": "mm/items/magic_beans_none"},
+    # { "predicate": { "custom_model_data": 8000022}, "model": "mm/items/powder_keg_none"},
+
+
+item_names = ['bomb_none', 'deku_nut_none', 'powder_keg_none', 'deku_stick_none', 'magic_beans_none', 'bombchu_none']ls
 
 for item_name in item_names:
     f = open(item_name + '.json', 'w')
